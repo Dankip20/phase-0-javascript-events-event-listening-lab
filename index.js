45783c3ed07e7844c40784cbb41499f63f1688e0
dan20 <kiptoodaniel92@gmail.com>
2024-04-11 09:13:53 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function addingEventListener() {
-    const input=document.getElementById("button");
-    input.addEventListener("click",function(){
-        alert("i was clicked");
-    });
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
